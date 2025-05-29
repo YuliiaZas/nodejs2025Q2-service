abf6b033e702a457441ef0e8fb9fcaf701e56519
YuliiaZas <julia.kv101@gmail.com>
2025-05-29 13:38:59 -0400

    init: use port from .env file

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import 'dotenv/config';
 // import { join, resolve } from 'node:path';
 // import { existsSync, mkdirSync, writeFileSync } from 'node:fs';
 // import * as yaml from 'yaml';
@@ -27,6 +28,6 @@ async function bootstrap() {
 
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(4000);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
